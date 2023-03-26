ef44e51f5caf5b96ce5fd2819dfda0b0497c9e02
Simon Schaep <simon.schaep@gmail.com>
2023-03-26 13:35:56 +0200

    changes

--- GameEngine/RotateComponent.cpp
+++ GameEngine/RotateComponent.cpp
@@ -8,12 +8,7 @@ void RotateComponent::Initialize()
 
 void RotateComponent::Update()
 {
-	const float dt{ Time::GetInstance().GetDeltaTime() };
-	m_Angle += dt * m_RotateSpeed;
+	m_Angle += Time::GetInstance().GetDeltaTime() * m_RotateSpeed;
 
-	glm::vec3 pos{ m_Offset };
-	
-	pos.x += m_Radius * cosf(m_Angle);
-	pos.y += m_Radius * sinf(m_Angle);
-	GetGameObject()->GetTransform()->SetLocalPosition(pos);
+	GetGameObject()->GetTransform()->SetLocalPosition({ m_Radius * cosf(m_Angle), m_Radius * sinf(m_Angle) });
 }
--- GameEngine/RotateComponent.h
+++ GameEngine/RotateComponent.h
@@ -11,12 +11,10 @@ public:
 	virtual void Initialize() override;
 	virtual void Update() override;
 
-	void SetOffset(const glm::vec3& pos) { m_Offset = pos; };
 	void SetRotateSpeed(float rotateSpeed) { m_RotateSpeed = rotateSpeed; };
 	void SetRadius(float radius) { m_Radius = radius; };
 
 private:
-	glm::vec3 m_Offset{};
 	float m_Angle{}; //radians
 	float m_RotateSpeed{30}; //radians per second
 	float m_Radius{30};
